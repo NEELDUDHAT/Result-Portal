4ed5fd2f4b1c23e717611a9d9c9c6b7f8ce4a527
Neel Dudhat <neelhareshbhaidudhat@gmail.com>
2023-08-22 23:58:47 +0530

    result portal index page

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Result Web Portal</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div align="center">
+        <br><br>
+        <span class="head">Online Result Display </span>
+        <hr class="hr">
+        <br>
+        <br>
+        <span class="subhead">View Result</span>
+        <br>
+        <br>
+        <br>
+        <form method="post" action="viewresult.php">
+            <table border="0" cellpadding="5" cellspacing="5" class="design">
+                <tr>
+                    <td colspan="2" align="center" class="msg">
+                        <!-- <?php echo $msg; ?> -->
+                    </td>
+                </tr>
+                <tr>
+                    <td class="labels">Roll No.:</td>
+                    <td><input type="text" required="required" name="roll" class="fields" size="15"
+                            placeholder="Enter Roll No." /></td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center"><input type="submit" value="SHOW" class="fields" /></td>
+                </tr>
+            </table>
+        </form>
+        <br><br>
+        <a href="register.php" class="link">Student Register</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="faculty.php" class="link">Faculty Login</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="registerFaculty.php" class="link">Faculty Registration</a>
+
+
+    </div>
+</body>
+
+</html>
